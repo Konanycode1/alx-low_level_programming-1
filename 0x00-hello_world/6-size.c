d2e7066e2bfa24c157f6c4db767f8c244e1b5aac
konanycode1 <konanytic@gmail.com>
2022-06-30 23:11:26 -0700

    okkk

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ long int li;
 long long int lli;
 float d;
 char c;
-printf("Size of char: %lu byte(s)\n", sizeof(c));
-print("Size of an int: %lu byte(s)\n", sizeof(i));
-printf("Size of a long int: %lu byte(s)\n", sizeof(li));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
-printf("Size of a float: %lu byte(s)\n", sizeof(d));
+printf("Size of char: %d byte(s)\n", sizeof(c));
+print("Size of an int: %d byte(s)\n", sizeof(i));
+printf("Size of a long int: %d byte(s)\n", sizeof(li));
+printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+printf("Size of a float: %d byte(s)\n", sizeof(d));
 return (0);
 
 }
