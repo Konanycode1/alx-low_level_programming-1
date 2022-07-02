fa2055f6b79f55d0672386838e77a30526d29b16
konanycode1 <konanytic@gmail.com>
2022-07-02 04:50:41 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - Print all possible combinations of two two-digit numbers
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+int nbr1, nbr2;
+for (nbr1 = 0; nbr2 < 9; nbr1++)
+{
+	for (nbr2 = nbr1 + 1; nbr2 < 10; nbr2++)
+	{
+		putchar((nbr1 % 10) + '0');
+		putchar((nbr2 % 10) + '0');
+		if (nbr1 == 8 && nbr2 == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
