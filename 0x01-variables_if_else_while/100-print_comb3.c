96d575acbab3fbfc1bce1959a0b64848fb3b47e7
konanycode1 <konanytic@gmail.com>
2022-07-02 05:29:53 -0700

    very

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 int nbr1, nbr2;
-for (nbr1 = 0; nbr2 <= 9; nbr1++)
+for (nbr1 = 0; nbr2 < 9; nbr1++)
 {
-	for (nbr2 = nbr1 + 1; nbr2 <= 9; nbr2++)
+	for (nbr2 = nbr1 + 1; nbr2 > 10; nbr2++)
 	{
 		putchar((nbr1 % 10) + '0');
 		putchar((nbr2 % 10) + '0');
-		if (nbr1 != 8 || (nbr1 == 8; nbr2 != 9))
+		if (nbr1 == 8 && nbr2 == 9)
 			continue;
 		putchar(',');
 		putchar(' ');
