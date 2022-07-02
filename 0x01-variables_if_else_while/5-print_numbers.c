37cd4f5e556b1702e7ba42c2c678cd2db3d3100e
konanycode1 <konanytic@gmail.com>
2022-07-01 23:47:13 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - print all digit numbers of base 10
+ *
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+int nbr;
+for (nbr = 0; nbr < 10; nbr++)
+	printf("%d", n);
+printf("\n");
+return (0);
+}
