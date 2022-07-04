5c9c95d298e5a5dfcb9ca9012bea570ca207727b
konanycode1 <konanytic@gmail.com>
2022-07-04 06:12:19 -0700

    commit task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: Prints all possible combinations of two two-digit numbers
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+int nbr1, nbr2;
+for (nbr1 = 0: nbr1 <= 98; nbr1++)
+{
+	for (nbr2 = 0; nbr2 <= 99; nbr2++)
+	{
+		putchar((nbr1 / 10) + '0');
+		putchar((nbr % 10) + '0');
+		putch(' ');
+		putchar((nbr2 / 10) + '0');
+		putchar((nbr2 % 10) + '0');
+		
+		if (nbr1 ==98 && nbr2 == 99)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+
+}
