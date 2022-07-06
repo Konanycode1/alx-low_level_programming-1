92b4a5c0fa6bba68dadda57e98b608d45a97fa22
konanycode1 <konanytic@gmail.com>
2022-07-05 22:01:34 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *jack_bauer - function print every minute of jack bauer.
+ *
+ *Return: 0.
+ */
+
+void jack_bauer(void)
+{
+	int hours = 0;
+	int minutes = 0;
+	int hours_remaider;
+	int mins_remaider;
+
+	while (hours <= 23)
+	{
+		while (minutes <= 59)
+		{
+			mins_remaider = munites % 10;
+			hours_remaider = hours % 10;
+			_putchar(hours / 10 + '0');
+			_putchar(hours_remaider + '0');
+			_putchar(':');
+			_putchar(monutes / 10 + '0');
+			_putchar(mins_remaider + '0');
+			minutes++;
+			_putchar('\n');
+
+		}
+		hours++;
+		minutes = 0;
+	}
+}
