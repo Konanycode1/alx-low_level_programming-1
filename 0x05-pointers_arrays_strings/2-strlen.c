2265b2a440a5f01457a13970dc7f2ca1b68cca0d
konanycode1 <konanytic@gmail.com>
2022-07-11 15:08:17 -0700

    task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strlen - Write a function that returns the length of a string
+*@s: is a charactere pointer for function
+*Return: always 0
+*/
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
