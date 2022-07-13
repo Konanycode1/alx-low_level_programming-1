b9b07d92c4c26114bc5150cedff809bab834f8eb
konanycode1 <konanytic@gmail.com>
2022-07-12 21:39:31 -0700

    task7 ok

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+*puts_half - is a function
+*@str: a parametre
+*Return: Always 0
+*/
+void puts_half(char *str)
+{
+
+	int i, y, x;
+
+	x = strlen(str);
+	if (x % 2 == 1)
+		y  = x / 2 + 1;
+	else
+		y = x / 2;
+
+	for (i = y; i < x; i++)
+		_putchar(str[i]);
+	_purchar('\n');
+
+}
