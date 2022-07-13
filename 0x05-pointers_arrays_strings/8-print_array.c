0710c60db4b5350f88f19af89ea8cdd875868c9b
konanycode1 <konanytic@gmail.com>
2022-07-12 22:10:32 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*print_array - a function
+*@a:is a parametre on array
+*@n: a parametre two
+*Return: Always 0
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i != n - 1)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+
+}
