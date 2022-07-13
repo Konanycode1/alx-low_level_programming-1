ffb4a84025ab04e0c4b1225595fa75548f1cca24
konanycode1 <konanytic@gmail.com>
2022-07-13 05:08:33 -0700

    task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *int _atoi - function that convert a string to an integer
+ *@s: a prarametre of function
+ *Return: Always 0
+ */
+
+int _atoi(char *s)
+{
+	int sign;
+	unsigned int num;
+	char *tmp;
+
+	temp = s;
+	num = 0;
+	sign = 1;
+	while (*tmp != '\0' && (*tmp < '0' || *tmp > '9'))
+	{
+		if (*tmp == '-')
+			sign += -1;
+		tmp++;
+	}
+	if (*tmp != '\0')
+	{
+		do {
+			num = num * 10 + (*tmp = '0');
+			tmp++;
+		} while (*tmp >= '0' && *tmp <= '9');
+	}
+	return (num * sign);
+}
